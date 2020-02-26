063862ab93018aac123410daedd51c655bd75a9a
Kazuhiro Hayashi <zuka0828@gmail.com>
2020-02-27 02:00:18 +0900

    Split a single LAVA job into multiple parts
    common.yml: Shared by all jobs
    device_*: Device dependent job definitions
    test_*: Test definitions
    
    .gitlab-ci.yml needs to submit jobs by
    specifying "device" and "test" names above.
    
    Signed-off-by: Kazuhiro Hayashi <zuka0828@gmail.com>

--- lava/submit-job.sh
+++ lava/submit-job.sh
@@ -6,15 +6,19 @@
 
 set -e
 
+LAVA_URI="https://${LAVA_USER}:${LAVA_TOKEN}@lava.ciplatform.org/RPC2"
+
 usage() {
-	echo "Usage: ${0} <job>"
+	echo "Usage: ${0} <device_type> <test>"
 	exit 1
 }
 
-if [ -z "${1}" ]; then
+if [ -z "${2}" ]; then
 	usage
 fi
-JOB=${1}
+device_type="${1}"
+test="${2}"
+job_name="cip-core-buster-deby-${device_type}-${test}"
 
 #if [ "$CI_COMMIT_REF_NAME" != "master" ]; then
 #	exit 0
@@ -28,5 +32,15 @@ if ! which lavacli 2>&1 >/dev/null; then
 	pip3 install --user lavacli
 fi
 
-echo "Submitting job ${JOB}"
-lavacli --uri https://${LAVA_USER}:${LAVA_TOKEN}@lava.ciplatform.org/RPC2 jobs submit ${JOB}
+echo "Compiling job ${job_name}"
+cat lava/jobs/common.yml \
+    lava/jobs/device_${device_type}.yml \
+    lava/jobs/test_${test}.yml \
+    > ${job_name}.yml
+sed -i "s|device_type|${device_type}|g" ${job_name}.yml
+sed -i "s|job_name|${job_name}|g" ${job_name}.yml
+echo "${job_name}.yml:"
+cat ${job_name}.yml
+
+echo "Submitting job ${job_name}"
+lavacli --uri ${LAVA_URI} jobs submit ${job_name}.yml
